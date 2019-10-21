2efcf7f881ee7890a35a0ec018474c18bb65ad87
Steven Palmer-Jones <spalmerjones@gmail.com>
2019-10-21 23:33:11 +0100

    sdfsdf

--- src/main/java/com/example/demo/Override.java
+++ src/main/java/com/example/demo/Override.java
@@ -1,8 +1,9 @@
 package com.example.demo;
 
 import java.util.Map.Entry;
+import java.util.function.Supplier;
 
 public interface Override {
-    Entry<String, String> get();
+    Entry<String, Supplier<String>> get();
     String applyTo(String content);
 }
--- src/main/java/com/example/demo/config/NamedOverride.java
+++ src/main/java/com/example/demo/config/NamedOverride.java
@@ -7,26 +7,27 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.function.Supplier;
 
 @Getter
 public enum NamedOverride implements Override {
-    RANDOM_ORDER_ID(Map.entry("${ORDER_ID}", "sfsdfsd")),
+    RANDOM_ORDER_ID(Map.entry("${ORDER_ID}", () -> "sfsdfsd")),
     SUCCESSFUL_CARD_TOKEN(Map.entry("${CARD_TOKEN}", "sdfsdfsdfsdfds")),
     FAILURE_CARD_TOKEN(Map.entry("${CARD_TOKEN}", "sdfsdfsdfsdfds"));
 
-    private Entry<String, String> override;
+    private Entry<String, Supplier<String>> override;
 
-    NamedOverride(Entry<String, String> override) {
+    NamedOverride(Entry<String, Supplier<String>> override) {
         this.override = override;
     }
 
     @java.lang.Override
-    public Entry<String, String> get() {
+    public Entry<String, Supplier<String>> get() {
         return override;
     }
 
     public String applyTo(String content) {
-        return content.replace(this.override.getKey(), this.override.getValue());
+        return content.replace(this.override.getKey(), this.override.getValue().get());
     }
 
     public static List<Override> DEFAULTS = new ArrayList<>();
